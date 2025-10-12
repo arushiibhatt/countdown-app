7e30e30e579b5eead904df91bd211b871745768f
Arushi <arushi.k.bhatt@gmail.com>
2025-10-12 16:28:09 -0400

    Initial setup with React and Tailwind CSS

---
+++ countdown/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
